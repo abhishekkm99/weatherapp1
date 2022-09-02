3779d5650f5aa025163dfbe6189a70f7a129f542
Abhishek Kumar <89724622+abhishekkm99@users.noreply.github.com>
2022-09-02 18:35:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-/**
- * Weather App
- * TODO: Complete getWeatherData() to return json response Promise
- * TODO: Complete searchCity() to get user input and get data using getWeatherData()
- * TODO: Complete showWeatherData() to set the data in the the html file from response
- */
-
-/* DIV ID's you'll need access to 👇
-"city-name"
-"weather-type"
-"temp"
-"min-temp"
-"max-temp"
-*/
 
 // API_KEY for maps api
 let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
 /**
  * Retrieve weather data from openweathermap
- * HINT: Use fetch()
  * HINT: URL should look like this: 
  * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
  */
@@ -28,28 +13,20 @@ getWeatherData = (city) => {
   fetch(`${URL}/?q=${city}&appid=${API_KEY}&units=imperial`)
   .then((response) => response.json())
   .then((data) => showWeatherData(data))
-
-  //CODE GOES HERE
 }
 
 /**
  * Retrieve city input and get the weather data
- * HINT: Use the promise returned from getWeatherData()
  */
 const searchCity = () => {
   const city = document.getElementById('city-input').value;
-  // console.log(city)
-  // CODE GOES HERE
   getWeatherData(city)
-
 }
 
 /**
  * Show the weather data in HTML
- * HINT: make sure to console log the weatherData to see how the data looks like
  */
 const showWeatherData = (weatherData) => {
-  //CODE GOES HERE
 const cityName = document.getElementById('city-name')
 const weatherType = document.getElementById('weather-type')
 const temp = document.getElementById('temp')  
